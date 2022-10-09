44365763e10b4ea3112b9e725d544df5800636b8
Fungx <38498093+Fungx@users.noreply.github.com>
2022-10-09 14:55:27 +0800

    x

--- optimize.py
+++ optimize.py
@@ -1,7 +1,9 @@
+import math
+
 from scipy.optimize import minimize, Bounds
 import numpy as np
 
-EPSILON = 1e-6
+EPSILON = 1e-5
 
 
 class OptimizedResult:
@@ -115,6 +117,14 @@ def energy(variables: np.ndarray, ws, wc) -> float:
     return contrast(variables) * penalty(variables, ws, wc)
 
 
+# def energy_sa3(variables: np.ndarray, ws, wc) -> float:
+#     return contrast(variables) - 1 / penalty(variables, ws, wc)
+
+
+def combination_num(n, i) -> int:
+    return math.factorial(n) / (math.factorial(i) * math.factorial(n - i))
+
+
 def optimize_sa1(k: int, init_variables=None, maxitr=1_000_000, esp_p=0.5, esp_r=0.5, initial_temp=2000.0,
                  terminated_temp=0.0001, alpha=0.99, ws=25, wc=1):
     """
@@ -133,9 +143,13 @@ def optimize_sa1(k: int, init_variables=None, maxitr=1_000_000, esp_p=0.5, esp_r
     """
     # randomly initialize
     # [[p0],[p1],[r]]
-    prev_variables = np.array([np.random.dirichlet(np.ones(k + 1)), np.random.dirichlet(np.ones(k + 1)),
+    prev_variables = np.array([[combination_num(k, i) / (2 ** (k - 1)) if i % 2 == 0 else 0 for i in range(k + 1)],
+                               [combination_num(k, i) / (2 ** (k - 1)) if i % 2 != 0 else 0 for i in range(k + 1)],
                                np.random.uniform(0, 1, k + 1)]) if init_variables is None \
         else init_variables
+
+    assert abs(sum(prev_variables[0]) - 1) < EPSILON and abs(sum(prev_variables[1]) - 1) < EPSILON and \
+           (0 <= prev_variables[2].all() <= 1)
 
     temp = initial_temp
     itr_cnt = 0
@@ -191,10 +205,173 @@ def optimize_sa1(k: int, init_variables=None, maxitr=1_000_000, esp_p=0.5, esp_r
     return OptimizedResult(True, itr_cnt, contrast(best_variables), best_variables)
 
 
+def optimize_sa2(k: int, init_variables=None, maxitr=10, markov=2000, esp_p=0.5, esp_r=0.5, initial_temp=2000.0,
+                 terminated_temp=0.001, alpha=0.95, ws=25, wc=1):
+    """
+    模拟退火
+    :param k: 阈值
+    :param init_variables: 初始变量
+    :param maxitr: 最大迭代次数
+    :param markov: 马尔可夫链长度，也就是每次迭代，在同一温度下搜索的次数
+    :param esp_p: 更新p0,p1的参数
+    :param esp_r: 更新r的参数
+    :param initial_temp: 初始温度
+    :param terminated_temp: 结束温度
+    :param alpha: 降温系数，越大降温越慢
+    :param ws: 惩罚函数的安全性系数
+    :param wc: 惩罚函数的对比度系数
+    :return:
+    """
+    # randomly initialize
+    # [[p0],[p1],[r]]
+    prev_variables = np.array([[combination_num(k, i) / (2 ** (k - 1)) if i % 2 == 0 else 0 for i in range(k + 1)],
+                               [combination_num(k, i) / (2 ** (k - 1)) if i % 2 != 0 else 0 for i in range(k + 1)],
+                               np.random.uniform(0, 1, k + 1)]) if init_variables is None \
+        else init_variables
+    assert abs(sum(prev_variables[0]) - 1) < EPSILON and abs(sum(prev_variables[1]) - 1) < EPSILON and \
+           (0 <= prev_variables[2].all() <= 1)
+    temp = initial_temp
+    itr_cnt = 0
+    best_energy = energy(prev_variables, ws, wc)
+    best_variables = prev_variables.copy()
+    prev_energy = best_energy
+
+    search_cnt = 0;
+    # start sa
+    while itr_cnt < maxitr and temp > terminated_temp:
+        for _ in range(markov):
+            new_variables = prev_variables.copy()
+            # randomly alter new_variables (k+1)//2 times
+            for _ in range((k + 1) // 2):
+                # alter p0,p1
+                i1, i2 = np.random.choice(k + 1, 2, False)
+                p_color = new_variables[np.random.choice(2)]  # randomly choose p0 or p1
+                tmp_esp = np.random.uniform(0, esp_p)  # [0,esp_p)
+                chosen_esp = min(tmp_esp, 1 - p_color[i1], p_color[i2])
+                p_color[i1] += chosen_esp
+                p_color[i2] -= chosen_esp
+                # alter r
+                j1 = np.random.choice(k + 1)
+                tmp_esp = np.random.uniform(-esp_r, esp_r)
+                r_j1 = new_variables[2][j1]
+                r_j1 += tmp_esp
+                if r_j1 > 1:
+                    r_j1 = 1
+                elif r_j1 < 0:
+                    r_j1 = 0
+                new_variables[2][j1] = r_j1
+
+            # calc new_energy
+            new_energy = energy(new_variables, ws, wc)
+
+            # set up next search
+            delta = new_energy - prev_energy
+            if delta > 0 or np.random.random() < np.exp(delta / temp):
+                # receive new variables as the next start point of searching
+                prev_variables = new_variables
+                prev_energy = new_energy
+                # update best variables
+                if prev_energy > best_energy:
+                    best_variables = prev_variables.copy()
+                    best_energy = prev_energy
+                    itr_cnt = 0
+            # else use previous variables to search again
+
+            # check if all variables still in bounds
+            assert abs(sum(prev_variables[0]) - 1) < EPSILON and abs(sum(prev_variables[1]) - 1) < EPSILON and \
+                   (0 <= prev_variables[2].all() <= 1)
+            search_cnt += 1
+        # update params
+        itr_cnt += 1
+        temp = temp * alpha
+    return OptimizedResult(True, search_cnt, contrast(best_variables), best_variables)
+
+
+def optimize_sa3(k: int, init_variables=None, maxitr=10, markov=2000, esp_p=0.5, esp_r=0.5, initial_temp=2000.0,
+                 terminated_temp=0.001, alpha=0.95, ws=50, wc=1):
+    """
+    模拟退火
+    :param k: 阈值
+    :param init_variables: 初始变量
+    :param maxitr: 最大迭代次数
+    :param markov: 马尔可夫链长度，也就是每次迭代，在同一温度下搜索的次数
+    :param esp_p: 更新p0,p1的参数
+    :param esp_r: 更新r的参数
+    :param initial_temp: 初始温度
+    :param terminated_temp: 结束温度
+    :param alpha: 降温系数，越大降温越慢
+    :param ws: 惩罚函数的安全性系数
+    :param wc: 惩罚函数的对比度系数
+    :return:
+    """
+
+    # randomly initialize
+    # [[p0],[p1],[r]]
+    prev_variables = np.array([[combination_num(k, i) / (2 ** (k - 1)) if i % 2 == 0 else 0 for i in range(k + 1)],
+                               [combination_num(k, i) / (2 ** (k - 1)) if i % 2 != 0 else 0 for i in range(k + 1)],
+                               np.random.uniform(0, 1, k + 1)]) if init_variables is None \
+        else init_variables
+    assert abs(sum(prev_variables[0]) - 1) < EPSILON and abs(sum(prev_variables[1]) - 1) < EPSILON and \
+           (0 <= prev_variables[2].all() <= 1)
+    temp = initial_temp
+    itr_cnt = 0
+    best_energy = energy(prev_variables, ws, wc)
+    best_variables = prev_variables.copy()
+    prev_energy = best_energy
+
+    search_cnt = 0;
+    # start sa
+    while itr_cnt < maxitr and temp > terminated_temp:
+        for _ in range(markov):
+            new_variables = prev_variables.copy()
+            # randomly alter new_variables (k+1)//2 times
+            for _ in range((k + 1) // 2):
+                # alter p0,p1
+                i1, i2 = np.random.choice(k + 1, 2, False)
+                p_color = new_variables[np.random.choice(2)]  # randomly choose p0 or p1
+                tmp_esp = np.random.uniform(0, esp_p)  # [0,esp_p)
+                chosen_esp = min(tmp_esp, 1 - p_color[i1], p_color[i2])
+                p_color[i1] += chosen_esp
+                p_color[i2] -= chosen_esp
+                # alter r
+                j1 = np.random.choice(k + 1)
+                tmp_esp = np.random.uniform(-esp_r, esp_r)
+                r_j1 = new_variables[2][j1]
+                r_j1 += tmp_esp
+                if r_j1 > 1:
+                    r_j1 = 1
+                elif r_j1 < 0:
+                    r_j1 = 0
+                new_variables[2][j1] = r_j1
+
+            # calc new_energy
+            new_energy = energy(new_variables, ws, wc)
+
+            # set up next search
+            delta = new_energy - prev_energy
+            if delta > 0 or np.random.random() < np.exp(delta / temp):
+                # receive new variables as the next start point of searching
+                prev_variables = new_variables
+                prev_energy = new_energy
+                # update best variables
+                if prev_energy > best_energy:
+                    best_variables = prev_variables.copy()
+                    best_energy = prev_energy
+                    itr_cnt = 0
+            # else use previous variables to search again
+
+            # check if all variables still in bounds
+            assert abs(sum(prev_variables[0]) - 1) < EPSILON and abs(sum(prev_variables[1]) - 1) < EPSILON and \
+                   (0 <= prev_variables[2].all() <= 1)
+            search_cnt += 1
+        # update params
+        itr_cnt += 1
+        temp = temp * alpha
+    return OptimizedResult(True, search_cnt, contrast(best_variables), best_variables)
+
+
 if __name__ == '__main__':
-    k = 2
-    vs = np.array([[1 / 3, 0, 2 / 3], [0, 1, 0], [0, 2 / 3, 1]])
-    result = optimize_sa1(k)
+    result = optimize_sa3(3,initial_temp=0.1,terminated_temp=0.1/2000,ws=50)
     # result = optimize_sa1(k, init_variables=vs)
     # print(_contrast(vs))
     # print(_penalty(vs, 1, 1))
